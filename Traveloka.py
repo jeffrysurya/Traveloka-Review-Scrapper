7cc98d22dc8f8ff8f8498da4d97b1bb8132b22f6
JSD <suryadharma2@gmail.com>
2022-12-04 15:20:11 +0700

    Separate English and Indonesia Review : Done

--- Traveloka.py
+++ Traveloka.py
@@ -42,16 +42,47 @@ def saveReview(data):
     with open('review.json', 'a') as f:
         json.dump(data, f, indent=4)
 
-review_data = []
-for i in range(0,1510,10):
-    data = getTravelokaReview(i)
-    for i in range(len(data)):
-        review_data.append({
-            'Name': data[i]['reviewerName'],
-            'Score': data[i]['overallScore'],
-            'Review': data[i]['reviewText'],
-        })
+# review_data = []
+# for i in range(0,1510,10):
+#     data = getTravelokaReview(i)
+#     for i in range(len(data)):
+#         review_data.append({
+#             'Name': data[i]['reviewerName'],
+#             'Score': data[i]['overallScore'],
+#             'Review': data[i]['reviewText'],
+#         })
 
-with open('review.json', 'w') as f:
-    json.dump(review_data, f, indent=4)
+# with open('review.json', 'w') as f:
+#     json.dump(review_data, f, indent=4)
+
+english_Review = []
+english_count = 0
+indonesia_Review = []
+indonesia_count = 0
+for page in range(0,1510,10):
+    data = getTravelokaReview(page)
+    for review in range(len(data)):
+        if data[review]['language'] == 'ENGLISH':
+            english_Review.append({
+                'Name': data[review]['reviewerName'],
+                'Score': data[review]['overallScore'],
+                'Review': data[review]['reviewText'],
+            })
+            english_count += 1
+        else:
+            indonesia_Review.append({
+                'Name': data[review]['reviewerName'],
+                'Score': data[review]['overallScore'],
+                'Review': data[review]['reviewText'],
+            })
+            indonesia_count += 1
+
+print('English Review: ', english_count)
+print('Indonesia Review: ', indonesia_count)
+
+with open('Englsih.json', 'w') as f:
+    json.dump(english_Review, f, indent=4)
+
+with open('Indonesia.json', 'w') as f:
+    json.dump(indonesia_Review, f, indent=4)
 
